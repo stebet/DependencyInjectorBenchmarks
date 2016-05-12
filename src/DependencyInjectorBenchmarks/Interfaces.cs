670d8779805ce72dee86eb0125fb7cd9ca6c058e
Stefán Jökull Sigurðarson <stefanjokull@hotmail.com>
2016-05-12 00:33:00 +0000

    Initial checkin.

---
+++ src/DependencyInjectorBenchmarks/Interfaces.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectorBenchmarks
+{
+    public interface IStorage
+    {
+        int Add(int a, int b);
+        int Subtract(int a, int b);
+        int Multiply(int a, int b);
+        int Divide(int a, int b);
+    }
+
+    public class Storage : IStorage
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+    public class SingletonStorage : Storage
+    {
+        public static readonly IStorage Instance = new Storage();
+    }
+}
---
+++ src/DependencyInjectorBenchmarks/Program.cs
@@ -0,0 +1,69 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnostics.Windows;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectorBenchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<DIBenchmarks>();
+        }
+    }
+
+    [Config(typeof(Config))]
+    public class DIBenchmarks
+    {
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                Add(Job.Default
+                    .WithLaunchCount(3)     // benchmark process will be launched only once
+                    .WithIterationTime(100) // 100ms per iteration
+                    .WithWarmupCount(3)     // 3 warmup iteration
+                    .WithTargetCount(3)     // 3 target iteration
+                    );
+
+                Add(new MemoryDiagnoser());
+            }
+        }
+
+        Container simpleInjectorContainer = new Container();
+
+        public DIBenchmarks()
+        {
+            simpleInjectorContainer.Register(typeof(IStorage), typeof(Storage), Lifestyle.Transient);
+        }
+
+        [Benchmark(Baseline = true)]
+        public int DirectTransient()
+        {
+            IStorage storage = new Storage();
+            return storage.Add(1, 2);
+        }
+
+        [Benchmark]
+        public int DirectSingleton()
+        {
+            IStorage storage = SingletonStorage.Instance;
+            return storage.Add(1, 2);
+        }
+
+        [Benchmark]
+        public int SimpleInjectorTransient()
+        {
+            IStorage storage = simpleInjectorContainer.GetInstance<IStorage>();
+            return storage.Add(1, 2);
+        }
+    }
+}
