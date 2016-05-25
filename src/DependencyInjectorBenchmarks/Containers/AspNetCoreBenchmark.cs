270a5ab23f4fc0c02579f0e7c77322988ac3f42e
Ben Adams <thundercat@illyriad.co.uk>
2016-05-25 02:20:04 +0100

    Add AspNetCore DI

---
+++ src/DependencyInjectorBenchmarks/Containers/AspNetCoreBenchmark.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using DependencyInjectorBenchmarks.Scenarios;
+
+namespace DependencyInjectorBenchmarks.Containers
+{
+    public class AspNetCoreBenchmark : IContainerBenchmark
+    {
+        public static readonly AspNetCoreBenchmark Instance = new AspNetCoreBenchmark();
+
+        private readonly IServiceProvider provider;
+
+        public AspNetCoreBenchmark()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddSingleton(Singleton.Instance);
+            serviceCollection.AddTransient<ITransient, Transient>();
+            serviceCollection.AddTransient<ICombined, Combined>();
+
+            provider = serviceCollection.BuildServiceProvider();
+        }
+
+        public ICombined ResolveCombined() => provider.GetService<ICombined>();
+
+        public ISingleton ResolveSingleton() => provider.GetService<ISingleton>();
+
+        public ITransient ResolveTransient() => provider.GetService<ITransient>();
+    }
+}
