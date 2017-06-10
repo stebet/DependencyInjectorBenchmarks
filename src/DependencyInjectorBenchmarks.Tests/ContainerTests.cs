708d41898e67c331655cb92bbecbdf968229465e
Stefán J. Sigurðarson <stefanj@ccpgames.com>
2017-06-10 00:45:23 +0000

    Migrating from XUnit to MSTestV2

--- src/DependencyInjectorBenchmarks.Tests/ContainerTests.cs
+++ src/DependencyInjectorBenchmarks.Tests/ContainerTests.cs
@@ -4,70 +4,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DependencyInjectorBenchmarks.Tests
 {
+    [TestClass]
     public class ContainerTests
     {
-        [Fact]
+        [TestMethod]
         public void TestSingleton()
         {
-            Assert.NotNull(DirectBenchmark.Instance.ResolveSingleton());
-            Assert.NotNull(NinjectBenchmark.Instance.ResolveSingleton());
-            Assert.NotNull(SimpleInjectorBenchmark.Instance.ResolveSingleton());
-            Assert.NotNull(AutofacBenchmark.Instance.ResolveSingleton());
-            Assert.NotNull(AspNetCoreBenchmark.Instance.ResolveSingleton());
-            Assert.NotNull(LightInjectBenchmark.Instance.ResolveSingleton());
-            Assert.NotNull(StructureMapBenchmark.Instance.ResolveSingleton());
+            Assert.IsNotNull(DirectBenchmark.Instance.ResolveSingleton());
+            Assert.IsNotNull(NinjectBenchmark.Instance.ResolveSingleton());
+            Assert.IsNotNull(SimpleInjectorBenchmark.Instance.ResolveSingleton());
+            Assert.IsNotNull(AutofacBenchmark.Instance.ResolveSingleton());
+            Assert.IsNotNull(AspNetCoreBenchmark.Instance.ResolveSingleton());
+            Assert.IsNotNull(LightInjectBenchmark.Instance.ResolveSingleton());
+            Assert.IsNotNull(StructureMapBenchmark.Instance.ResolveSingleton());
 
-            Assert.Same(DirectBenchmark.Instance.ResolveSingleton(), DirectBenchmark.Instance.ResolveSingleton());
-            Assert.Same(NinjectBenchmark.Instance.ResolveSingleton(), NinjectBenchmark.Instance.ResolveSingleton());
-            Assert.Same(SimpleInjectorBenchmark.Instance.ResolveSingleton(), SimpleInjectorBenchmark.Instance.ResolveSingleton());
-            Assert.Same(AutofacBenchmark.Instance.ResolveSingleton(), AutofacBenchmark.Instance.ResolveSingleton());
-            Assert.Same(AspNetCoreBenchmark.Instance.ResolveSingleton(), AspNetCoreBenchmark.Instance.ResolveSingleton());
-            Assert.Same(LightInjectBenchmark.Instance.ResolveSingleton(), LightInjectBenchmark.Instance.ResolveSingleton());
-            Assert.Same(StructureMapBenchmark.Instance.ResolveSingleton(), StructureMapBenchmark.Instance.ResolveSingleton());
+            Assert.AreSame(DirectBenchmark.Instance.ResolveSingleton(), DirectBenchmark.Instance.ResolveSingleton());
+            Assert.AreSame(NinjectBenchmark.Instance.ResolveSingleton(), NinjectBenchmark.Instance.ResolveSingleton());
+            Assert.AreSame(SimpleInjectorBenchmark.Instance.ResolveSingleton(), SimpleInjectorBenchmark.Instance.ResolveSingleton());
+            Assert.AreSame(AutofacBenchmark.Instance.ResolveSingleton(), AutofacBenchmark.Instance.ResolveSingleton());
+            Assert.AreSame(AspNetCoreBenchmark.Instance.ResolveSingleton(), AspNetCoreBenchmark.Instance.ResolveSingleton());
+            Assert.AreSame(LightInjectBenchmark.Instance.ResolveSingleton(), LightInjectBenchmark.Instance.ResolveSingleton());
+            Assert.AreSame(StructureMapBenchmark.Instance.ResolveSingleton(), StructureMapBenchmark.Instance.ResolveSingleton());
         }
 
-        [Fact]
+        [TestMethod]
         public void TestTransient()
         {
-            Assert.NotNull(DirectBenchmark.Instance.ResolveTransient());
-            Assert.NotNull(NinjectBenchmark.Instance.ResolveTransient());
-            Assert.NotNull(SimpleInjectorBenchmark.Instance.ResolveTransient());
-            Assert.NotNull(AutofacBenchmark.Instance.ResolveTransient());
-            Assert.NotNull(AspNetCoreBenchmark.Instance.ResolveTransient());
-            Assert.NotNull(LightInjectBenchmark.Instance.ResolveTransient());
-            Assert.NotNull(StructureMapBenchmark.Instance.ResolveTransient());
+            Assert.IsNotNull(DirectBenchmark.Instance.ResolveTransient());
+            Assert.IsNotNull(NinjectBenchmark.Instance.ResolveTransient());
+            Assert.IsNotNull(SimpleInjectorBenchmark.Instance.ResolveTransient());
+            Assert.IsNotNull(AutofacBenchmark.Instance.ResolveTransient());
+            Assert.IsNotNull(AspNetCoreBenchmark.Instance.ResolveTransient());
+            Assert.IsNotNull(LightInjectBenchmark.Instance.ResolveTransient());
+            Assert.IsNotNull(StructureMapBenchmark.Instance.ResolveTransient());
 
-            Assert.NotSame(DirectBenchmark.Instance.ResolveTransient(), DirectBenchmark.Instance.ResolveTransient());
-            Assert.NotSame(NinjectBenchmark.Instance.ResolveTransient(), NinjectBenchmark.Instance.ResolveTransient());
-            Assert.NotSame(SimpleInjectorBenchmark.Instance.ResolveTransient(), SimpleInjectorBenchmark.Instance.ResolveTransient());
-            Assert.NotSame(AutofacBenchmark.Instance.ResolveTransient(), AutofacBenchmark.Instance.ResolveTransient());
-            Assert.NotSame(AspNetCoreBenchmark.Instance.ResolveTransient(), AspNetCoreBenchmark.Instance.ResolveTransient());
-            Assert.NotSame(LightInjectBenchmark.Instance.ResolveTransient(), LightInjectBenchmark.Instance.ResolveTransient());
-            Assert.NotSame(StructureMapBenchmark.Instance.ResolveTransient(), StructureMapBenchmark.Instance.ResolveTransient());
+            Assert.AreNotSame(DirectBenchmark.Instance.ResolveTransient(), DirectBenchmark.Instance.ResolveTransient());
+            Assert.AreNotSame(NinjectBenchmark.Instance.ResolveTransient(), NinjectBenchmark.Instance.ResolveTransient());
+            Assert.AreNotSame(SimpleInjectorBenchmark.Instance.ResolveTransient(), SimpleInjectorBenchmark.Instance.ResolveTransient());
+            Assert.AreNotSame(AutofacBenchmark.Instance.ResolveTransient(), AutofacBenchmark.Instance.ResolveTransient());
+            Assert.AreNotSame(AspNetCoreBenchmark.Instance.ResolveTransient(), AspNetCoreBenchmark.Instance.ResolveTransient());
+            Assert.AreNotSame(LightInjectBenchmark.Instance.ResolveTransient(), LightInjectBenchmark.Instance.ResolveTransient());
+            Assert.AreNotSame(StructureMapBenchmark.Instance.ResolveTransient(), StructureMapBenchmark.Instance.ResolveTransient());
         }
 
-        [Fact]
+        [TestMethod]
         public void TestCombined()
         {
-            Assert.NotNull(DirectBenchmark.Instance.ResolveCombined());
-            Assert.NotNull(NinjectBenchmark.Instance.ResolveCombined());
-            Assert.NotNull(SimpleInjectorBenchmark.Instance.ResolveCombined());
-            Assert.NotNull(AutofacBenchmark.Instance.ResolveCombined());
-            Assert.NotNull(AspNetCoreBenchmark.Instance.ResolveCombined());
-            Assert.NotNull(LightInjectBenchmark.Instance.ResolveCombined());
-            Assert.NotNull(StructureMapBenchmark.Instance.ResolveCombined());
+            Assert.IsNotNull(DirectBenchmark.Instance.ResolveCombined());
+            Assert.IsNotNull(NinjectBenchmark.Instance.ResolveCombined());
+            Assert.IsNotNull(SimpleInjectorBenchmark.Instance.ResolveCombined());
+            Assert.IsNotNull(AutofacBenchmark.Instance.ResolveCombined());
+            Assert.IsNotNull(AspNetCoreBenchmark.Instance.ResolveCombined());
+            Assert.IsNotNull(LightInjectBenchmark.Instance.ResolveCombined());
+            Assert.IsNotNull(StructureMapBenchmark.Instance.ResolveCombined());
 
-            Assert.NotSame(DirectBenchmark.Instance.ResolveCombined(), DirectBenchmark.Instance.ResolveCombined());
-            Assert.NotSame(NinjectBenchmark.Instance.ResolveCombined(), NinjectBenchmark.Instance.ResolveCombined());
-            Assert.NotSame(SimpleInjectorBenchmark.Instance.ResolveCombined(), SimpleInjectorBenchmark.Instance.ResolveCombined());
-            Assert.NotSame(AutofacBenchmark.Instance.ResolveCombined(), AutofacBenchmark.Instance.ResolveCombined());
-            Assert.NotSame(AspNetCoreBenchmark.Instance.ResolveCombined(), AspNetCoreBenchmark.Instance.ResolveCombined());
-            Assert.NotSame(LightInjectBenchmark.Instance.ResolveCombined(), LightInjectBenchmark.Instance.ResolveCombined());
-            Assert.NotSame(StructureMapBenchmark.Instance.ResolveCombined(), StructureMapBenchmark.Instance.ResolveCombined());
+            Assert.AreNotSame(DirectBenchmark.Instance.ResolveCombined(), DirectBenchmark.Instance.ResolveCombined());
+            Assert.AreNotSame(NinjectBenchmark.Instance.ResolveCombined(), NinjectBenchmark.Instance.ResolveCombined());
+            Assert.AreNotSame(SimpleInjectorBenchmark.Instance.ResolveCombined(), SimpleInjectorBenchmark.Instance.ResolveCombined());
+            Assert.AreNotSame(AutofacBenchmark.Instance.ResolveCombined(), AutofacBenchmark.Instance.ResolveCombined());
+            Assert.AreNotSame(AspNetCoreBenchmark.Instance.ResolveCombined(), AspNetCoreBenchmark.Instance.ResolveCombined());
+            Assert.AreNotSame(LightInjectBenchmark.Instance.ResolveCombined(), LightInjectBenchmark.Instance.ResolveCombined());
+            Assert.AreNotSame(StructureMapBenchmark.Instance.ResolveCombined(), StructureMapBenchmark.Instance.ResolveCombined());
         }
     }
 }
